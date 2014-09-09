fff4e15056f2882122c411979fd002064773cb16
haiyangl <haiyangl@codeaurora.org>
2014-09-09 13:52:47 +0800

    SystemUI: Add task manager
    Add a button in status bar expanded header to switch to task manager,
    which supports to watch current running tasks and usage of memory, also
    supports to close one or all of running tasks.
    
    Change-Id: Ic06df8171e741c2ead63e077421c172624094ab0
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    CAF Task Manager switch [1/2]
    
    Change-Id: I4b7c3041bfa74bcd7e2981a39a34860422f596fc
    
    Fixed Task Manager bug described in post below
    
    https://plus.google.com/101691799763674764197/posts/Z7XFJSfsCYz
    
    Change-Id: If4f3f3c67c80cd8be3111ea3dbc3ac1f3b6d7618
    
    Expose task manager values for themers
    
    Change-Id: I843b366875aa2dc5b5f8f2e705c0ca81b71d1f1a
    
    Task manager improvements
    
    * add ripple animation to task manager button
    * add rotation animation to task manager button and clean up heads up button
    * use the existing SettingsObservers so enabling/disabling is done properly
    * fix a nullpointer in PhonStatusBar at mTaskManager.refreshTaskManagerView();
    
    Change-Id: I784a725ed8a0fb87425618f6d0513720293904a1
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    SystemUI : Fix Task Manager strings
    
    Signed-off-by: dexmc <dextermcdew@gmail.com>
    
    Change-Id: I8859259fdfdd055ae1adb204383a81dacea728c0
    
    Update some Task Manager functions
    
    - Simplify the ram usage code
    
    Change-Id: I8a36fe7cecc1499707486d23b7a51f6773c9aeae
    
    Task Manager : On switch recreate statusbar to fix task view
    
    Change-Id: I0fd66cc0c50674f6a6e6e0f88104f3bf1507e9df
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Task Manager: Use one observer for changes, misc cleanup
    
    -- We use just one observer and propagate values to interested classes with setter methods
    
    -- Remove some null checks. We create the TaskManagerPanel regardless now.
    
    -- Explicitly set proper panel visibility state when feature becomes disabled
       while the TaskManager panel was visible
    
    -- Explicitly set TaskManager button click state to false when disabled
    
    -- Clean up StatusBarHeaderView SystemIcon LayoutParams for TeamEos implementation
    
    -- Survive a theme change
    
    Change-Id: I0f772e4743e4fa357f13f4ae705bc136ac9dffa3
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Reset to QS panel if task manager was visible last
    
    *also don't allow task manager to show on lock screen
    
    Change-Id: Iabb1a7a19d0b57c99c1c01fbeec2a3d7549e432f
    
    Materialize task kill button
    
    Change-Id: I44242954fe98cec6f777ff1b00e88bd105b2d1e9
    (cherry picked from commit 0a37cce48dc2f5b84b8cb83052d90a446e34ad9c)
    
    SystemUI:Add Missing Parts for CAF Task Manager
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Fix Task manager icon layout and get rid of QS emergency calls only text
    
    Update task manager icon
    
    Add emergency calls only string

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -54,6 +54,7 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 import android.view.animation.PathInterpolator;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.logging.MetricsLogger;
@@ -252,6 +253,11 @@ public class NotificationPanelView extends PanelView implements
     private int mStatusBarHeaderHeight;
     private GestureDetector mDoubleTapGesture;
 
+    // Task manager
+    private boolean mShowTaskManager;
+    private boolean mTaskManagerShowing;
+    private LinearLayout mTaskManagerPanel;
+
     public NotificationPanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(!DEBUG);
@@ -281,6 +287,7 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardStatusView = (KeyguardStatusView) findViewById(R.id.keyguard_status_view);
         mQsContainer = (QSContainer) findViewById(R.id.quick_settings_container);
         mQsPanel = (QSPanel) findViewById(R.id.quick_settings_panel);
+        mTaskManagerPanel = (LinearLayout) findViewById(R.id.task_manager_panel);
         mClockView = (TextView) findViewById(R.id.clock_view);
         mScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
         mScrollView.setListener(this);
@@ -844,6 +851,7 @@ public class NotificationPanelView extends PanelView implements
         if (action == MotionEvent.ACTION_DOWN && isFullyCollapsed()
                 && mQsExpansionEnabled) {
             mTwoFingerQsExpandPossible = true;
+            mStatusBar.resetQsPanelVisibility();
         }
         if (mTwoFingerQsExpandPossible && isOpenQsEvent(event)
                 && event.getY(event.getActionIndex()) < mStatusBarMinHeight) {
@@ -1409,7 +1417,6 @@ public class NotificationPanelView extends PanelView implements
         mNotificationStackScroller.setScrollingEnabled(
                 mStatusBarState != StatusBarState.KEYGUARD && (!mQsExpanded
                         || mQsExpansionFromOverscroll));
-        mQsPanel.setVisibility(expandVisually ? View.VISIBLE : View.INVISIBLE);
         mQsContainer.setVisibility(
                 mKeyguardShowing && !expandVisually ? View.INVISIBLE : View.VISIBLE);
         mScrollView.setTouchEnabled(mQsExpanded);
@@ -1442,6 +1449,7 @@ public class NotificationPanelView extends PanelView implements
         updateNotificationScrim(height);
         if (mKeyguardShowing) {
             updateHeaderKeyguard();
+            mStatusBar.resetQsPanelVisibility();
         }
         if (mStatusBarState == StatusBarState.SHADE_LOCKED
                 || mStatusBarState == StatusBarState.KEYGUARD) {
@@ -1628,6 +1636,37 @@ public class NotificationPanelView extends PanelView implements
             return onHeader || (mScrollView.isScrolledToBottom() && yDiff < 0) && isInQsArea(x, y);
         } else {
             return onHeader;
+        }
+    }
+
+    void setTaskManagerEnabled(boolean enabled) {
+        mShowTaskManager = enabled;
+        // explicity restore visibility states when disabled
+        // and TaskManager last state was showing
+        if (!enabled && mTaskManagerShowing) {
+            mTaskManagerShowing = false;
+            mQsPanel.setVisibility(View.VISIBLE);
+            mTaskManagerPanel.setVisibility(View.GONE);
+        }
+    }
+
+    public void setTaskManagerVisibility(boolean taskManagerShowing) {
+        if (mShowTaskManager) {
+            mTaskManagerShowing = taskManagerShowing;
+            cancelAnimation();
+            boolean expandVisually = mQsExpanded || mStackScrollerOverscrolling;
+            mQsPanel.setVisibility(expandVisually && !taskManagerShowing
+                    ? View.VISIBLE : View.GONE);
+            mTaskManagerPanel.setVisibility(expandVisually && taskManagerShowing
+                    && !mKeyguardShowing ? View.VISIBLE : View.GONE);
+        }
+    }
+
+
+
+  private void cancelAnimation() {
+        if (mQsExpansionAnimator != null) {
+            mQsExpansionAnimator.cancel();
         }
     }
 
@@ -2550,7 +2589,7 @@ public class NotificationPanelView extends PanelView implements
             resolver.registerContentObserver(CMSettings.System.getUriFor(
                     CMSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN), false, this);
             resolver.registerContentObserver(CMSettings.System.getUriFor(
-                    CMSettings.System.DOUBLE_TAP_SLEEP_GESTURE), false, this);
+                    CMSettings.System.DOUBLE_TAP_SLEEP_GESTURE), false, this);        
             update();
         }
 
@@ -2589,7 +2628,6 @@ public class NotificationPanelView extends PanelView implements
         } else if (source == StatusBarManager.CAMERA_LAUNCH_SOURCE_WIGGLE) {
             mLastCameraLaunchSource = KeyguardBottomAreaView.CAMERA_LAUNCH_SOURCE_WIGGLE;
         } else {
-
             // Default.
             mLastCameraLaunchSource = KeyguardBottomAreaView.CAMERA_LAUNCH_SOURCE_AFFORDANCE;
         }
