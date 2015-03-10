0b341606fba4481d26a94df4b7b4271898f1cca2
Goran Jakovljevic <goran.jakovljevic@imgtec.com>
2015-03-10 17:26:01 +0100

    Generate gnu hash for arm only
    Fixes build failure caused by:
    https://android-review.googlesource.com/#/c/139670/
    
    As previously discussed [1], MIPS does not support GNU-style ELF hashes.
    
    [1] https://android-review.googlesource.com/#/c/49282/2/tests/Android.mk
    
    Change-Id: I8edb8a1595308db2ecd1bd6650a069b1224ae64f
    
    Conflicts:
    	native/graphics/jni/Android.mk

--- native/graphics/jni/Android.mk
+++ native/graphics/jni/Android.mk
@@ -28,5 +28,10 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_MODULE:= libjnigraphics
 
+LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
+
+# TODO: This is to work around b/19059885. Remove after root cause is fixed
+LOCAL_LDFLAGS_arm := -Wl,--hash-style=both
+
 include $(BUILD_SHARED_LIBRARY)
 
