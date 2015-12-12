ce14756f768cf0e63a8ec5a63a20d193ec370e71
jgcaap <jgcaap@gmail.com>
2015-12-11 22:04:41 -0500

    lets apply a different threslhold for scrolling cache to get a smoother experience
    Change-Id: Ie94921c06ceae61eb01e32b46f971d7bbaa04f36
    
    Conflicts:
    	core/java/android/widget/AbsListView.java

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -864,7 +864,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         mTouchSlop = configuration.getScaledTouchSlop();
         mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
         mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
-        mDecacheThreshold = mMaximumVelocity / 2;
+        mDecacheThreshold = mMaximumVelocity * 3 / 2;
         mOverscrollDistance = configuration.getScaledOverscrollDistance();
         mOverflingDistance = configuration.getScaledOverflingDistance();
 
